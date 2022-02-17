a9ad5a9145f349cebaeeeb0101b93bbfdb27d184
Alejandro Villar Rubio <alvill04@ucm.es>
2022-02-17 17:11:03 +0100

    Added project and final game

---
+++ UnityProject/ECOlegio/Assets/uAdventure/__Scripts/Runner/Appearance/HideInventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using uAdventure.Runner;
+public class HideInventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        if (Game.Instance.GameState.CurrentTarget == "Menu" || Game.Instance.GameState.CurrentTarget == "NotasFinales") InventoryManager.Instance.Show = false;
+	}
+	
+}
